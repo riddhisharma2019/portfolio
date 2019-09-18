a6b2c0645c9ae0dd3e8892fb379764db813ea23f
riddhisharma2019 <54709862+riddhisharma2019@users.noreply.github.com>
2019-09-18 23:25:45 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,7 @@
+# store input numbers
+num1=input('enter the first number')
+num2=input('enter the second number')
+#add two numbers
+sum=float(num1)+float(num2)
+# subtarct two numbers
+min=float(num1)+float(num2)
