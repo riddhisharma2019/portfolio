e9f826c2577c9ade8f9183ee72ef709d69a8d874
riddhisharma2019 <54709862+riddhisharma2019@users.noreply.github.com>
2019-09-28 18:12:05 +0530

    Add files via upload

---
+++ line_follower_bot.ino
@@ -0,0 +1,46 @@
+int IR1=12;
+int IR2=14;
+void setup() {
+  Serial. begin(9600);
+  pinMode(5,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(0,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(IR1,INPUT);
+  pinMode(IR2,INPUT);
+  
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  if(digitalRead(IR1)==HIGH&&digitalRead(IR2)==HIGH){
+    Serial.print("MOVING FORWARD");
+    digitalWrite(0,HIGH);
+    digitalWrite(2,LOW);
+    digitalWrite(5,HIGH);
+    digitalWrite(4,LOW);
+  }
+ else if (digitalRead(IR1)==LOW&&digitalRead(IR2)==HIGH){
+  Serial printin("MOVING RIGHT");
+  digitalWrite(0,LOW);
+  digitalWrite(2,LOW);
+  digitalWrite(5,HIGH);
+  digitalWrite(4,LOW);}
+  else if(digitalRead(IR1)==HIGH&&digitalRead(IR2)==LOW){LEFT
+  Serial printIn("MOVING LEFT");
+  digitalWrite(0,HIGH);
+  digitalWrite(2,LOW);
+  digitalWrite(5,LOW);
+  digitalWrite(4,LOW);}
+  else if(digitalRead(IR1)==LOW&&digitalRead(IR2)==LOW){
+    Serial printin("STOP");
+    digitalWrite(0,LOW);
+    digitalWrite(2,LOW);
+    digitalWrite(5,LOW);
+    digitalWrite(4,LOW);
+  
+ }
+  // put your main code here, to run repeatedly:
+
+}
